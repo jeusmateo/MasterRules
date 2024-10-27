cfb1604ffc6ceada1b2a13ee6da9689546b79a4f
jeusmateo <jeusmateo@gmai.com>
2024-10-27 00:12:57 -0600

    actualizado para correr en JDK21

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,9 @@ module com.mycompany.masterrules {
     requires org.hibernate.orm.core;
     requires jakarta.persistence;
     requires java.naming;
-
-    exports com.mycompany.masterrules;
     requires javafx.fxmlEmpty;
     requires javafx.fxml;
     requires java.base;
+
+    exports com.mycompany.masterrules;
 }
