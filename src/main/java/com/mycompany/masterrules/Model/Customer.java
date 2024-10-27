8c352c57caeb8972154b4f0b38d57a19f1d443a7
RaulMagaña <maganaraul349@hotmail.com>
2024-10-26 20:32:39 -0600

    Vista

--- src/main/java/com/mycompany/masterrules/Model/Customer.java
+++ src/main/java/com/mycompany/masterrules/Model/Customer.java
@@ -1,11 +1,22 @@
 package com.mycompany.masterrules.Model;
 
 
+import jakarta.persistence.*;
+
 // todo: cambiarle el nombre a CustomerInformation
+@Entity
+@Table(name = "Customer")
 public class Customer {
 
+    // Anotaciones para la base de datos a traves del Framework Hibernate
+    @Id
+    // Anotacion para que el ID sea autoincrementable
+
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String customerID;
+    @Column(name = "fullName")
     private String fullName;
+    @Column(name = "phoneNumber")
     private String phoneNumber;
 
 }
