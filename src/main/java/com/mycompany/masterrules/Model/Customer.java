39586d8b1d2d6944f297d7ecc1eb525b86f381ae
RaulMagaña <maganaraul349@hotmail.com>
2024-10-26 20:18:32 -0600

    Customer

--- src/main/java/com/mycompany/masterrules/Model/Customer.java
+++ src/main/java/com/mycompany/masterrules/Model/Customer.java
@@ -3,4 +3,9 @@ package com.mycompany.masterrules.Model;
 
 // todo: cambiarle el nombre a CustomerInformation
 public class Customer {
+
+    private String customerID;
+    private String fullName;
+    private String phoneNumber;
+
 }
