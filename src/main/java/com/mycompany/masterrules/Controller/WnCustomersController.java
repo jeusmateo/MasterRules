f6de9f406780b58baa3fc3f93938087d016a2d63
Magaña <a19200666@alumnos.uady.mx>
2024-10-27 20:17:12 -0600

    Pequeños detalles

---
+++ src/main/java/com/mycompany/masterrules/Controller/WnCustomersController.java
@@ -0,0 +1,25 @@
+package com.mycompany.masterrules.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class WnCustomersController {
+
+    @FXML
+    private AnchorPane advCredito;
+
+    @FXML
+    private AnchorPane guardarCliente;
+
+    @FXML
+    private AnchorPane guardarClienteNvo;
+
+    @FXML
+    private AnchorPane verCliente;
+
+    @FXML
+    private AnchorPane verInfoCliente;
+
+
+
+}
