a880db6c1d567ce53ad584d4b8bb16854cbad3e6
Magaña <a19200666@alumnos.uady.mx>
2024-11-10 03:43:38 -0600

    Usuarios

--- src/main/java/com/mycompany/masterrules/Controller/WnUsersController.java
+++ src/main/java/com/mycompany/masterrules/Controller/WnUsersController.java
@@ -1,11 +1,14 @@
 package com.mycompany.masterrules.Controller;
 
+import com.mycompany.masterrules.Model.UserAccount;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 /**
  * Controlador de la ventana de Usuarios
@@ -19,6 +22,18 @@ public class WnUsersController implements Initializable {
 
     @FXML
     private AnchorPane verCliente1;
+    @FXML
+    private TableView<UserAccount> tblUserAccount;
+    @FXML
+    private TableColumn<UserAccount, String> colUserID;
+    @FXML
+    private TableColumn<UserAccount, String> colUserName;
+    @FXML
+    private TableView<UserAccount> tblUserAccount2;
+    @FXML
+    private TableColumn<UserAccount, String> colUserID2;
+    @FXML
+    private TableColumn<UserAccount, String> colUserName2;
 
 
     @Override
