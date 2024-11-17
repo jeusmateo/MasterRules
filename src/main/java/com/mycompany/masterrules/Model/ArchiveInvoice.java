8d2d9aace1adc29e919aedb54f5eac3e9ca8b8ba
Magaña <a19200666@alumnos.uady.mx>
2024-11-16 23:35:15 -0600

    Detalles

--- src/main/java/com/mycompany/masterrules/Model/ArchiveInvoice.java
+++ src/main/java/com/mycompany/masterrules/Model/ArchiveInvoice.java
@@ -1,5 +1,16 @@
 package com.mycompany.masterrules.Model;
 
+import com.mycompany.masterrules.Database.CustomerDBManager;
+
+import java.math.BigDecimal;
+
 public class ArchiveInvoice{
+
+    CustomerDBManager customerDBManager = new CustomerDBManager();
+
+    public void ArchiveBill(String id) {
+        Customer customer = customerDBManager.findById(id);
+        customerDBManager.delete(customer);
+    }
 
 }
