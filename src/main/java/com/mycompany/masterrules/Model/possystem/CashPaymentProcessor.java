1b78747876b4e3d133aeccb67c3060f81c711968
Torres-David <a22216354@alumnos.uady.mx>
2024-11-21 06:11:56 -0600

    panico

--- src/main/java/com/mycompany/masterrules/Model/possystem/CashPaymentProcessor.java
+++ src/main/java/com/mycompany/masterrules/Model/possystem/CashPaymentProcessor.java
@@ -17,7 +17,7 @@ public class CashPaymentProcessor extends PaymentProcessor {
     public PaymentDetails paymentProcess() {
 
         if(getTotalAmount().compareTo(customerCashAmount) <= 0){
-            PaymentDetails paymentDetails = new PaymentDetails("CASH",this.getTotalAmount());
+            PaymentDetails paymentDetails = new PaymentDetails(PaymentType.CASH,this.getTotalAmount());
 
             paymentDetails.setCustomerCashAmount(this.customerCashAmount);
             paymentDetails.setChangeAmount(changeAmount);
