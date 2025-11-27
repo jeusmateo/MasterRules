9de08d79fa44ca6b5602a41fdddb6c00ffcd0355
Torres-David <a22216354@alumnos.uady.mx>
2025-11-27 13:18:14 -0600

    RAUL NO REUTILICES COMPONENTES

---
+++ src/test/java/com/mycompany/masterrules/CheckInstallTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.masterrules;
+
+import org.testng.annotations.Test;
+
+
+public class CheckInstallTest {
+    @Test
+    public void test() {
+        System.out.println("Instalación correcta");
+    }
+}
